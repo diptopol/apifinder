e6f4ab2ce94ff0176cbb0e1fc860ad933b1b2f6d
Diptopol Dam <diptodam@gmail.com>
2022-05-01 19:07:06 -0400

    Performed minor refactoring
    - Extracted a metod
    - Removed unnecessary try catch block
    - Removed unused methods

--- src/main/java/ca/concordia/jaranalyzer/models/ClassInfo.java
+++ src/main/java/ca/concordia/jaranalyzer/models/ClassInfo.java
@@ -1,8 +1,11 @@
 package ca.concordia.jaranalyzer.models;
 
-import ca.concordia.jaranalyzer.models.typeInfo.*;
-import ca.concordia.jaranalyzer.util.signaturevisitor.ClassSignatureFormalTypeParameterExtractor;
+import ca.concordia.jaranalyzer.models.typeInfo.ParameterizedTypeInfo;
+import ca.concordia.jaranalyzer.models.typeInfo.PrimitiveTypeInfo;
+import ca.concordia.jaranalyzer.models.typeInfo.QualifiedTypeInfo;
+import ca.concordia.jaranalyzer.models.typeInfo.TypeInfo;
 import ca.concordia.jaranalyzer.util.InferenceUtility;
+import ca.concordia.jaranalyzer.util.signaturevisitor.ClassSignatureFormalTypeParameterExtractor;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.apache.tinkerpop.gremlin.structure.VertexProperty;
 import org.objectweb.asm.Opcodes;
@@ -16,7 +19,6 @@ import org.objectweb.asm.tree.MethodNode;
 import java.util.*;
 
 public class ClassInfo {
-
     private static final String ANONYMOUS_INNER_CLASS_NAME_REGEX = ".*\\$[0-9]+";
 
     private String qualifiedName;
@@ -68,93 +70,88 @@ public class ClassInfo {
     }
 
     public ClassInfo(ClassNode classNode) {
-        try {
-            this.methods = new ArrayList<>();
-            this.fields = new ArrayList<>();
-            this.innerClassNameList = new ArrayList<>();
-            this.superInterfaceMap = new LinkedHashMap<>();
-            this.qualifiedName = classNode.name.replace('/', '.');
-            this.signature = classNode.signature;
+        this.methods = new ArrayList<>();
+        this.fields = new ArrayList<>();
+        this.innerClassNameList = new ArrayList<>();
+        this.superInterfaceMap = new LinkedHashMap<>();
+        this.signature = classNode.signature;
 
-            this.isInnerClass = qualifiedName.contains("$");
-            this.isAnonymousInnerClass = qualifiedName.matches(ANONYMOUS_INNER_CLASS_NAME_REGEX);
+        this.isInnerClass = classNode.name.contains("$");
+        this.isAnonymousInnerClass = classNode.name.matches(ANONYMOUS_INNER_CLASS_NAME_REGEX);
 
-            this.qualifiedName = qualifiedName.contains("$") ? qualifiedName.replace("$", ".") : qualifiedName;
-            if (!classNode.name.contains("/")) {
-                this.name = classNode.name;
-                this.packageName = "";
-            } else {
-                this.name = classNode.name.substring(
-                        classNode.name.lastIndexOf('/') + 1);
-                this.packageName = classNode.name.substring(0, classNode.name.lastIndexOf('/')).replace('/', '.');
+        this.qualifiedName = getQualifiedClassName(classNode.name);
+
+        if (!classNode.name.contains("/")) {
+            this.name = classNode.name;
+            this.packageName = "";
+        } else {
+            this.name = classNode.name.substring(
+                    classNode.name.lastIndexOf('/') + 1);
+            this.packageName = classNode.name.substring(0, classNode.name.lastIndexOf('/')).replace('/', '.');
+        }
+
+        if (classNode.superName != null) {
+            this.superClassName = getQualifiedClassName(classNode.superName);
+        } else {
+            this.superClassName = "";
+        }
+
+        this.type = Type.getObjectType(classNode.name);
+
+        int access = classNode.access;
+
+        /*
+         * access (for inner classes) does not reflact acces of classNode. Instead, we have to fetch access property
+         * of innerClassNode and use it.
+         */
+        for (InnerClassNode innerClassNode : classNode.innerClasses) {
+            if (innerClassNode.name.equals(classNode.name)) {
+                access = innerClassNode.access;
             }
+        }
 
-            if (classNode.superName != null) {
-                this.superClassName = classNode.superName.replace('/', '.').replace("$", ".");
-            } else {
-                this.superClassName = "";
+        if ((access & Opcodes.ACC_PUBLIC) != 0) {
+            isPublic = true;
+        } else if ((access & Opcodes.ACC_PROTECTED) != 0) {
+            isProtected = true;
+        } else if ((access & Opcodes.ACC_PRIVATE) != 0) {
+            isPrivate = true;
+        }
+
+        if ((access & Opcodes.ACC_ABSTRACT) != 0) {
+            isAbstract = true;
+        }
+
+        if ((access & Opcodes.ACC_INTERFACE) != 0) {
+            isInterface = true;
+        }
+
+        if ((access & Opcodes.ACC_ENUM) != 0) {
+            isEnum = true;
+        }
+
+        @SuppressWarnings("unchecked")
+        List<String> implementedInterfaces = classNode.interfaces;
+        for (String interfaceName : implementedInterfaces) {
+            superInterfaceMap.put(getQualifiedClassName(interfaceName), null);
+        }
+
+        @SuppressWarnings("unchecked")
+        List<MethodNode> methodNodes = classNode.methods;
+        for (MethodNode methodNode : methodNodes) {
+            methods.add(new MethodInfo(methodNode, this));
+        }
+
+        @SuppressWarnings("unchecked")
+        List<FieldNode> fieldNodes = classNode.fields;
+        for (FieldNode fieldNode : fieldNodes) {
+            fields.add(new FieldInfo(fieldNode, this));
+        }
+
+        for (InnerClassNode innerClassNode : classNode.innerClasses) {
+            if (!innerClassNode.name.matches(ANONYMOUS_INNER_CLASS_NAME_REGEX)) {
+                innerClassNameList.add(getQualifiedClassName(innerClassNode.name));
             }
-
-            this.type = Type.getObjectType(classNode.name);
-
-            int access = classNode.access;
-
-            /*
-             * access (for inner classes) does not reflact acces of classNode. Instead, we have to fetch access property
-             * of innerClassNode and use it.
-             */
-            for (InnerClassNode innerClassNode : classNode.innerClasses) {
-                if (innerClassNode.name.equals(classNode.name)) {
-                    access = innerClassNode.access;
-                }
-            }
-
-            if ((access & Opcodes.ACC_PUBLIC) != 0) {
-                isPublic = true;
-            } else if ((access & Opcodes.ACC_PROTECTED) != 0) {
-                isProtected = true;
-            } else if ((access & Opcodes.ACC_PRIVATE) != 0) {
-                isPrivate = true;
-            }
-
-            if ((access & Opcodes.ACC_ABSTRACT) != 0) {
-                isAbstract = true;
-            }
-
-            if ((access & Opcodes.ACC_INTERFACE) != 0) {
-                isInterface = true;
-            }
-
-            if ((access & Opcodes.ACC_ENUM) != 0) {
-                isEnum = true;
-            }
-
-            @SuppressWarnings("unchecked")
-            List<String> implementedInterfaces = classNode.interfaces;
-            for (String interfaceName : implementedInterfaces) {
-                superInterfaceMap.put(interfaceName.replace('/', '.').replace("$", "."), null);
-            }
-
-            @SuppressWarnings("unchecked")
-            List<MethodNode> methodNodes = classNode.methods;
-            for (MethodNode methodNode : methodNodes) {
-                methods.add(new MethodInfo(methodNode, this));
-            }
-
-            @SuppressWarnings("unchecked")
-            List<FieldNode> fieldNodes = classNode.fields;
-            for (FieldNode fieldNode : fieldNodes) {
-                fields.add(new FieldInfo(fieldNode, this));
-            }
-
-            for (InnerClassNode innerClassNode : classNode.innerClasses) {
-                if (!innerClassNode.name.matches(ANONYMOUS_INNER_CLASS_NAME_REGEX)) {
-                    innerClassNameList.add(innerClassNode.name.replace("/", ".").replace("$", "."));
-                }
-            }
-
-        } catch (Exception e) {
-            System.out.println(e);
         }
     }
 
@@ -227,15 +224,6 @@ public class ClassInfo {
         return innerClassNameList;
     }
 
-    public ArrayList<MethodInfo> getPublicMethods() {
-        ArrayList<MethodInfo> publicMethods = new ArrayList<>();
-        for (MethodInfo methodInfo : getMethods()) {
-            if (methodInfo.isPublic())
-                publicMethods.add(methodInfo);
-        }
-        return publicMethods;
-    }
-
     public String toString() {
         StringBuilder classDescription = new StringBuilder();
 
@@ -262,68 +250,8 @@ public class ClassInfo {
         return classDescription.toString();
     }
 
-    public List<FieldInfo> getFieldsByName(String fieldName) {
-        List<FieldInfo> matchedFields = new ArrayList<>();
-        for (FieldInfo fieldInfo : fields) {
-            if (fieldInfo.getName().equals(fieldName)) {
-                matchedFields.add(fieldInfo);
-            }
-        }
-
-        if (matchedFields.size() == 0 && superClassInfo != null) {
-            matchedFields.addAll(superClassInfo.getFieldsByName(fieldName));
-        }
-
-        if (matchedFields.size() == 0) {
-            for (String superInterfaceName : superInterfaceMap.keySet()) {
-                ClassInfo interfaceInfo = superInterfaceMap.get(superInterfaceName);
-                if (interfaceInfo != null) {
-                    matchedFields.addAll(interfaceInfo.getFieldsByName(fieldName));
-                }
-            }
-        }
-        return matchedFields;
-    }
-
     public List<FieldInfo> getFields() {
         return fields;
-    }
-
-    public List<MethodInfo> getMethods(String methodName,
-                                       int numberOfParameters) {
-        List<MethodInfo> matchedMethods = new ArrayList<>();
-
-        for (MethodInfo method : getMethods()) {
-            if (method.matches(methodName, numberOfParameters)) {
-                matchedMethods.add(method);
-            }
-        }
-
-        if (matchedMethods.size() == 0 && superClassInfo != null) {
-            matchedMethods.addAll(superClassInfo.getMethods(methodName, numberOfParameters));
-        }
-
-        if (matchedMethods.size() == 0) {
-            for (String superInterfaceName : superInterfaceMap.keySet()) {
-                ClassInfo interfaceInfo = superInterfaceMap.get(superInterfaceName);
-                if (interfaceInfo != null) {
-                    matchedMethods.addAll(interfaceInfo.getMethods(methodName, numberOfParameters));
-                }
-            }
-        }
-        return matchedMethods;
-    }
-
-    public boolean matchesImportStatement(String importedPackage) {
-        if (qualifiedName.replace('$', '.').startsWith(importedPackage)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public String getKind() {
-        return "CLASS";
     }
 
     public boolean isEnum() {
@@ -340,6 +268,14 @@ public class ClassInfo {
 
     public String getSignature() {
         return this.signature;
+    }
+
+    private String getQualifiedClassName(String className) {
+        String qualifiedClassName = className.replace('/', '.');
+
+        return qualifiedClassName.contains("$")
+                ? qualifiedClassName.replaceAll("\\$", ".")
+                : qualifiedClassName;
     }
 
     private TypeInfo getClassTypeInfo(Type type, String qualifiedName, String signature) {
